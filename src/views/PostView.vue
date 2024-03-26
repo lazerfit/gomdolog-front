6002207e13b175239422eb999a5bbfd0b88cb084
sg kim <redkafe98@gmail.com>
2024-03-26 23:34:24 +0900

    create PostView

---
+++ src/views/PostView.vue
@@ -0,0 +1,11 @@
+<script setup lang=ts>
+import MainHeader from '@/components/common/MainHeader.vue';
+
+</script>
+<template>
+  <div>
+    <main-header />
+  </div>
+</template>
+
+<style lang='scss' scoped></style>
