7ee7f5e2b577de58a493fc7df2587dd71c99eadf
sg kim <redkafe98@gmail.com>
2024-03-28 05:34:59 +0900

    update settings

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,21 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component }">
+    <Transition name="fade">
+      <component :is="Component" />
+    </Transition>
+  </RouterView>
 </template>
 
-<style scoped></style>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.4s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import PostSave from '@/components/PostSave.vue';
+
+const PostView = () => import('../views/PostView.vue');
+const CategoryView = () => import('../views/CategoryView.vue');
+const PostSaveView = () => import('../views/PostSaveView.vue');
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,8 +14,33 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/post/:id',
+      name: 'post',
+      component: PostView
+    },
+    {
+      path: '/category/:title',
+      name: 'category',
+      component: CategoryView
+    },
+    {
+      path: '/post/new',
+      name: 'postSave',
+      component: PostSaveView
+    }
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    } else {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve({ top: 0 });
+        }, 350);
+      });
+    }
+  }
+});
 
-  ]
-})
-
-export default router
+export default router;
