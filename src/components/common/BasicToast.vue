5933a306111b399f6cc76123b6d016ead0c596a9
sg kim <redkafe98@gmail.com>
2024-03-28 05:32:04 +0900

    created basic toast

---
+++ src/components/common/BasicToast.vue
@@ -0,0 +1,34 @@
+<script setup lang=ts>
+import { useToasterStore } from '@/stores/useToasterStore';
+
+const store = useToasterStore()
+
+</script>
+<template>
+  <Teleport to="body">
+    <div v-if="store.show" class="toast">
+      <i class="fa-solid fa-circle-check"></i>
+      {{ store.message }}
+    </div>
+  </Teleport>
+</template>
+
+<style lang='scss' scoped>
+.toast {
+  position: fixed;
+  top: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: px-to-rem(10) px-to-rem(20);
+  background-color: $black-forest;
+  color: $pearl;
+  border-radius: 10px;
+  box-shadow: 5px 5px 20px rgba($color: #333, $alpha: .25);
+  animation: simple-bounce .5s linear;
+
+  i {
+    margin-right: px-to-rem(5);
+    color: #2ecc71;
+  }
+}
+</style>
