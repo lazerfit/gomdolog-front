c22c784a82ae3435ff00998380e76580dad0ea45
sg kim <redkafe98@gmail.com>
2024-03-28 05:32:40 +0900

    create basic btn design

---
+++ src/components/common/MoreButton.vue
@@ -0,0 +1,27 @@
+<script setup lang=ts>
+
+</script>
+<template>
+  <div class="container">
+    <button>More Posts</button>
+  </div>
+</template>
+
+<style lang='scss' scoped>
+.container {
+  width: $desktop-width;
+  margin: px-to-rem(50) auto px-to-rem(150) auto;
+  display: flex;
+  justify-content: center;
+
+  button {
+    font-family: $secondary-font;
+    padding: px-to-rem(5);
+    border-radius: px-to-rem(5);
+    border: none;
+    background-color: $black-forest;
+    color: $pearl;
+    cursor: pointer;
+  }
+}
+</style>
