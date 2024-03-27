821a4bc8a47f13bef1e8215e4c3125d1a713d256
sg kim <redkafe98@gmail.com>
2024-03-28 05:33:47 +0900

    create category title banner

---
+++ src/components/CategoryTitle.vue
@@ -0,0 +1,53 @@
+<script setup lang=ts>
+import { useRoute, RouterLink } from 'vue-router';
+const route = useRoute();
+</script>
+<template>
+  <div class="container">
+    <div class="home">
+      <RouterLink to="/" class="fa-solid fa-house"></RouterLink>
+    </div>
+    <div class="category-title">
+      {{ route.params.title }}
+    </div>
+    <div class="post-count">
+      6 posts
+    </div>
+  </div>
+</template>
+
+<style lang='scss' scoped>
+.container {
+  width: $desktop-width;
+  margin: px-to-rem(50) auto;
+
+  .home {
+    text-align: center;
+
+    .fa-house {
+      color: $light-black;
+      cursor: pointer;
+      transition: all .5s ease;
+
+      &:hover {
+        color: $black-forest;
+      }
+    }
+
+  }
+
+  .category-title {
+    font-weight: 300;
+    font-family: $secondary-font;
+    font-size: px-to-rem(65);
+    text-align: center;
+  }
+
+  .post-count {
+    font-weight: 800;
+    font-family: $secondary-font-en;
+    font-size: px-to-rem(20);
+    text-align: center;
+  }
+}
+</style>
