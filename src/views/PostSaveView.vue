3f82c3040c5189ee2e0ad33a2253ea2993036b74
sg kim <redkafe98@gmail.com>
2024-04-02 22:09:41 +0900

    create post save

---
+++ src/views/PostSaveView.vue
@@ -0,0 +1,15 @@
+<script setup lang=ts>
+import MainHeader from '@/components/common/MainHeader.vue';
+import PostSave from '@/components/PostSave.vue';
+import TiptapEditor from '@/components/TiptapEditor.vue';
+import MainFooter from '@/components/common/MainFooter.vue';
+
+</script>
+<template>
+  <div class="container">
+    <main-header />
+    <tiptap-editor />
+  </div>
+</template>
+
+<style lang='scss' scoped></style>
