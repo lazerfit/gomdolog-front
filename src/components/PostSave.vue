19e576dd6432239bc907925e0e74586ce4030ab1
sg kim <redkafe98@gmail.com>
2024-04-02 22:08:29 +0900

    create post save page

---
+++ src/components/PostSave.vue
@@ -0,0 +1,18 @@
+<script setup lang=ts>
+
+</script>
+<template>
+  <div class="container">
+    <div class="wrapper">
+
+    </div>
+  </div>
+</template>
+
+<style lang='scss' scoped>
+.container {
+  width: $desktop-width;
+  margin: 0 auto;
+  border: 1px solid #000;
+}
+</style>
