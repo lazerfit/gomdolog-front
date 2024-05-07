70795a4b00337409fcedac320ba741ab608681af
sg kim <redkafe98@gmail.com>
2024-05-07 20:59:11 +0900

    feat: implementing login modal to mobile sidebar

--- src/components/MobileSidebar.vue
+++ src/components/MobileSidebar.vue
@@ -4,18 +4,66 @@ import { RouterLink } from 'vue-router';
 import { useCategoryResponseStore } from '@/stores/useCategoryStore';
 import { computed, onBeforeMount, ref, watchEffect } from 'vue';
 import { usePostResponseStore } from '@/stores/usePostResponseStore';
+import { useLoginStore } from '@/stores/useLoginStore';
 import { useRoute } from 'vue-router';
+import { defineAsyncComponent, onBeforeUnmount } from 'vue';
 
 const store = useCategoryResponseStore();
 const postStore = usePostResponseStore();
+const loginStore = useLoginStore();
 const route = useRoute();
 
 const filteredCategory = (computed(() => store.categories.filter(category => category.title !== '없음')))
 
+const isModalOpened = ref(false);
 const isBoxChecked = ref(false);
 
+const submitHandler = () => {
+  console.log("정상 제출 완료");
+}
+
+const BasicModal = defineAsyncComponent(() =>
+  import('@/components/common/BasicModal.vue')
+);
+
+const BasicToast = defineAsyncComponent(() =>
+  import('@/components/common/BasicToast.vue')
+);
+
+const openModal = () => {
+  isModalOpened.value = true;
+  isBoxChecked.value = false;
+}
+
+const closeModal = () => {
+  isModalOpened.value = false;
+}
+
+const signIn = () => {
+  loginStore.SIGN_IN()
+  closeModal()
+}
+
+const logout = () => {
+  loginStore.isAdmin = false;
+  sessionStorage.removeItem('userRole');
+}
+
+const isAdmin = () => {
+  if (sessionStorage.getItem('userRole') === null || sessionStorage.getItem('useRole') === 'USER') {
+    loginStore.isAdmin = false;
+  } else {
+    loginStore.isAdmin = true;
+  }
+}
+
 onBeforeMount(() => {
+  isAdmin()
   store.FETCH_ALL()
+})
+
+onBeforeUnmount(() => {
+  loginStore.isAdmin = false;
 })
 
 watchEffect(() => {
@@ -35,20 +83,21 @@ watchEffect(() => {
       <span></span>
       <span></span>
     </label>
+    <basic-toast />
     <div class="sidebar">
       <div class="searchbar-wrapper">
         <search-bar />
       </div>
       <div class="category-wrapper">
-        <div class="category" v-for="(item, index) in store.categories" :key="index">
+        <div class="category" v-for="(item, index) in filteredCategory" :key="index">
           {{ item }}
         </div>
       </div>
       <div class="login-wrapper">
-        <!-- <div class="login-btn">
+        <div class="login-btn" v-if="!loginStore.isAdmin" @click="openModal">
           로그인
-        </div> -->
-        <div class="admin-menu-wrapper">
+        </div>
+        <div class="admin-menu-wrapper" v-else>
           <div>
             <RouterLink to="/post/new">글쓰기</RouterLink>
           </div>
@@ -56,15 +105,56 @@ watchEffect(() => {
             <RouterLink to="/admin">설정</RouterLink>
           </div>
           <div>
-            <a href="#none">로그아웃</a>
+            <a href="#none" @click="logout">로그아웃</a>
           </div>
         </div>
       </div>
+      <Transition name="fade">
+        <basic-modal :isOpen="isModalOpened" @modal-close="closeModal" @submit="submitHandler">
+          <template #header>
+            <div class="modal-header-container">
+              <div class="close-btn">
+                <i class="fa-solid fa-x" @click="closeModal"></i>
+              </div>
+              <div class="login-welcome">
+                <img src="/src/assets/img/cat4.jpg" alt="login-welcome-img">
+              </div>
+            </div>
+          </template>
+          <template #body>
+            <div class="modal-body-container">
+              <div class="login-field">
+                <div>
+                  <input type="email" placeholder="이메일" required v-model="loginStore.signInForm.email">
+                </div>
+                <div>
+                  <input type="password" placeholder="비밀번호" required v-model="loginStore.signInForm.password">
+                </div>
+                <button class="btn-member-primary" @click="signIn">로그인</button>
+              </div>
+            </div>
+          </template>
+          <template #footer>
+            <div>
+            </div>
+          </template>
+        </basic-modal>
+      </Transition>
     </div>
   </div>
 </template>
 
 <style lang='scss' scoped>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity .5s ease-in-out;
+}
+
 .darkMode {
   label[for=trigger] {
     span {
@@ -214,6 +304,74 @@ watchEffect(() => {
             border: none;
             color: #B33939;
             @include box-shadow-animation(#b339398e);
+          }
+        }
+      }
+    }
+
+    .modal-header-container {
+      display: flex;
+      flex-direction: column;
+
+      .close-btn {
+        margin-left: auto;
+        color: $light-black;
+
+        i {
+          cursor: pointer;
+        }
+      }
+
+      .login-welcome {
+        img {
+          width: 100%;
+          border-radius: px-to-rem(20);
+          margin-top: px-to-rem(10);
+
+        }
+      }
+    }
+
+    .modal-body-container {
+      margin-bottom: 15px;
+
+      .login-field {
+        button {
+          cursor: pointer;
+        }
+
+        div {
+          :is(input[type=email], input[type=password]) {
+            border: 1px solid $dim-gray;
+            padding: px-to-rem(13);
+            border-radius: px-to-rem(5);
+            width: 100%;
+            margin-top: px-to-rem(10);
+            font-family: $secondary-font;
+
+            &::placeholder {
+              color: $dark-gray;
+              transition: all .3s ease;
+            }
+
+            &:focus::placeholder {
+              opacity: 0;
+            }
+          }
+        }
+
+        .btn-member-primary {
+          padding: px-to-rem(13);
+          border-radius: px-to-rem(5);
+          width: 100%;
+          margin-top: px-to-rem(10);
+          background-color: $black-forest;
+          color: $pearl;
+          transition: all .3s ease-in-out;
+          font-family: $secondary-font;
+
+          &:hover {
+            @include box-shadow-animation($silver-black);
           }
         }
       }
